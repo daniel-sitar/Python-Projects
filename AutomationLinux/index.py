dbe72a4e6dec279461d81762fd7012b41f48750c
daniel.sitar <sitarpublic@gmail.com>
2025-09-03 13:57:20 +0200

    add removing files

--- AutomationLinux/index.py
+++ AutomationLinux/index.py
@@ -3,12 +3,16 @@ from errno import ELIBACC
 
 print("What kind of task do you want to do?\nList files\nCreate new files or folders\nDelete documents\n")
 
+#Used to determine what the user wants to do
 task = str(input("\nType selected action\nList\nCreate\nDelete\n"))
+#Used to figure out where the user wants to perform their action
 path = str(input("\nWhere do you want to do the action\nHome\nDesktop\nDocuments\nDownloads\nPictures\nVideos\nRoot\n"))
+#Used to figure out the users home directory
 user = str(input("\nType in the name of your home directory\n"))
 
-#Lists files from the most common folders using ls.
+#Lists files from the desired path using ls.
 def listFiles(path,user):
+
     if path == "Home" or path == "home" or path == "HOME":
         os.system(f"ls /{path}")
     elif path == "Root" or path == "root" or path == "ROOT":
@@ -29,6 +33,7 @@ def listFiles(path,user):
 
 #Create new files or folders
 def createNew(path,user):
+
     executionType = str(input("\nDo you want to create a new file or folder?\nFile\nFolder\n"))
     if executionType == "Folder" or executionType == "folder" or executionType == "FOLDER":
         folderName = str(input("\nType in name of the folder you want to create\n"))
@@ -48,10 +53,15 @@ def createNew(path,user):
             os.system(f"mkdir /home/{user}/Videos/{folderName}")
         else:
             print("Path not found")
+
     elif executionType == "File" or executionType == "file" or executionType == "FILE":
+        #Used to figure out how many files the user wants
         amount = int(input("\nHow many files to create?\n"))
+        #Used to figure out what file type the user needs
         fileType = str(input("\nWhat kind of file do you want to create?\ntxt\n"))
+        #Used to figure out the name for the new files a _ is added + number of the file
         fileName = str(input("\nType in the name that you want for the files\n"))
+        #Loops in range of how many files there are supposed to be
         for x in range(1,amount+1):
             if path == "Home" or path == "home" or path == "HOME":
                 os.system(f"touch /$HOME/{fileName}_{x}.{fileType}")
@@ -70,10 +80,37 @@ def createNew(path,user):
             else:
                 print("Invalid path")
 
+
+#Deletes documents and folders
+def deleteContent(path,user):
+    deleteType = str(input("\nDo you want to delete files or a folder?\nFile\nFolder\n"))
+    if deleteType == "Folder" or deleteType == "folder" or deleteType == "FOLDER":
+        print("\nCAUTION : BE CAREFUL WHEN TYPING THE FOLDER NAME, DO NOT REMOVE SYSTEM FOLDERS\n")
+        folderName = str(input("\nType in name of the folder you want to delete\n"))
+        if path == "Home" or path == "home" or path == "HOME":
+            os.system(f"rm -r /$HOME/{folderName}")
+        elif path == "Root" or path == "root" or path == "ROOT":
+            print("Error : It is not wise to manipulate root folder in linux")
+        elif path == "Desktop" or path == "desktop" or path == "DESKTOP":
+            os.system(f"rm -r /home/{user}/Desktop/{folderName}")
+        elif path == "Documents" or path == "documents" or path == "DOCUMENTS":
+            os.system(f"rm -r /home/{user}/Documents/{folderName}")
+        elif path == "Downloads" or path == "downloads" or path == "DOWNLOADS":
+            os.system(f"rm -r /home/{user}/Downloads/{folderName}")
+        elif path == "Pictures" or path == "pictures" or path == "PICTURES":
+            os.system(f"rm -r /home/{user}/Pictures/{folderName}")
+        elif path == "Videos" or path == "videos" or path == "VIDEOS":
+            os.system(f"rm -r /home/{user}/Videos/{folderName}")
+        else:
+            print("Invalid path")
+
+
 #Executes selected function
 if task == "List" or task == "list" or task == "LIST":
     listFiles(path,user)
 elif task == "Create" or task == "create" or task == "CREATE":
     createNew(path,user)
+elif task == "Delete" or task == "delete" or task == "DELETE":
+    deleteContent(path,user)
 else:
     print("Invalid task")
